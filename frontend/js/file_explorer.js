71a0c4ad1e0d9bf10acb5cfda20c09711544ebfc
coimbrajoao <coimbrajoao235@gmail.com>
2026-05-30 14:30:15 -0300

    feat(frontend):ajustando icones no file explorer

--- frontend/js/file_explorer.js
+++ frontend/js/file_explorer.js
@@ -1,6 +1,27 @@
 import apiservice from "./api_service.js"
 
+
+const icones = {
+    py: '../assets/icons8-serpente-48.png',
+    js: '../assets/icons8-javascript-24.png',
+    css: '../assets/icons8-css-48.png',
+    java: '../assets/icons8-logo-java-coffee-cup-48.png',
+    txt: '../assets/icons8-logo-txt-50.png',
+    JSON: './assets/icons8-logo-json-50.png',
+    md: './assets/icons8-circled-i-48.png'
+}
+
+function pegarIcone(caminho){
+    const extensao = caminho.split('.').pop();
+
+    return icones[extensao]
+}
+
+
 export class FileExplorer{
+
+
+    
     
     async static list(){
       
@@ -18,8 +39,13 @@ export class FileExplorer{
 
             const titulo = document.createElement('h2')
 
+            const img = document.createElement('img')
+
             titulo.TextContent = element.text;
 
+            img.src = pegarIcone(titulo)
+
+            file.appendChild(img)
             file.appendChild(titulo)
 
             container.appendChild(file)
